7b29978802f9641ba5acc537bf8be9d824ef2ccf
Fia <rofiatul11248@gmail.com>
2025-05-24 23:38:45 +0700

    feat: add new components, styles, and service files

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: '/febe-last-project-web-intermedient-geolocation/',
   server: {
     port: 5173,
     strictPort: true,
